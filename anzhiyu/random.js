152c220d5cc3819d2e9e52c9dd0ff6e06ae56d6e
Xiao-xiaocn <Xiao-xiaocn@outlook.com>
2025-06-30 11:15:21 +0800

    Site updated: 2025-06-30 11:15:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/29/hello-world/"];function toRandomPost(){
+var posts=["2025/06/30/这是一篇新的博文测试1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
